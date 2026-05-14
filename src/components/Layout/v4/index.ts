3156cc04aec38c033e825c1f31919a375f52cf94
Jona <jona.glatz@gmail.com>
2026-05-14 17:15:01 +0200

    feat(v4): add v4 layout barrel re-export

---
+++ src/components/Layout/v4/index.ts
@@ -0,0 +1,8 @@
+/**
+ * v4 redesign layout primitives. Phase 1: navigation chrome.
+ * Future phases will add V4Card, V4Badge, V4StickyBar, V4AuthModal, etc.
+ */
+export { V4Logo } from './V4Logo';
+export { V4TopNav } from './V4TopNav';
+export { V4TopNavAuth } from './V4TopNavAuth';
+export { V4TabBar } from './V4TabBar';
